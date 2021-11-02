e83fb2ce4fa83ee0597ab438ccecc5c6fd2e1720
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2021-11-02 10:15:06 +0530

    Update numberPalindrome.cpp

--- numberPalindrome.cpp
+++ numberPalindrome.cpp
@@ -1,24 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int reverseNumber(int n) {
-	int ans = 0, rem, temp;
-	temp = n;
+int palindrome(int n) {
+	int ans = 0, rem;
+	// temp = n;
 	while(n > 0) {
 		rem = n%10;  // 6 3 1
 		n = n/10;    // 13 1 0
 		ans = ans*10+rem; // 6 63 631
 		
 	}
-	if(temp == ans)
-		return 1;
-	return 0;
+	return ans;
 }
 
 int main() {
 	int n;     // 136
 	cin >> n;
-	if(reverseNumber(n)) {
+	if(palindrome(n)) {
 		cout << "YES";
 	}
 	else
