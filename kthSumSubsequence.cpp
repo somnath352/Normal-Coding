4630e7dae8bc79c3c341f268261ff863186d6be0
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2022-01-03 21:07:43 +0530

    Update kthSumSubsequence.cpp

--- kthSumSubsequence.cpp
+++ kthSumSubsequence.cpp
@@ -1,42 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-// T.C => n * (2**n)
-// S.C => O(n)
-
-void seq(int ind, vector<int> &ds, int s, int sum, int arr[], int n) {
-	if(ind == n) {
-		if(s == sum) {
-			for(auto &it : ds) cout << it << " ";
+void seq(int ind, vector<int>& ds, int s, int sum, int n, int arr[]) {
+	if(ind==n) {
+		if(s==sum) {
+			for(auto &it: ds) {
+				cout << it <<" ";
+			}
 			cout << "\n";
 		}
 		return;
-
 	}
-
-
 	
-	// Not pick the element in the subsequence
-	seq(ind+1, ds, s, sum, arr, n);
-
-	// pick the element in the subsequence
-    ds.push_back(arr[ind]);
-    s += arr[ind];
-
-	seq(ind+1, ds, s, sum, arr, n);
-	
-	
+	// pick up the element
+	ds.push_back(arr[ind]);
+	s += arr[ind];
+	seq(ind+1, ds, s, sum, n, arr);
 	ds.pop_back();
 	s -= arr[ind];
+
+	// not pickup the element
+	seq(ind+1, ds, s, sum, n, arr);
 }
 
 int main() {
 	int arr[] = {1,2,1};
-	int n = sizeof(arr) / sizeof(arr[0]);
-	vector<int> ds;
+	int n = sizeof(arr)/sizeof(arr[0]);
 	int sum = 2;
-	seq(0, ds, 0, sum, arr, n);
-
-	return 0;
+	vector<int> ds;
+	seq(0, ds, 0, sum, n, arr);
 }
