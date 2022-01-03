4bf1b73621cf0424a1365fd1e1a4063eb57542af
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2022-01-03 21:19:51 +0530

    Update countSubsequenceSumK.cpp

--- countSubsequenceSumK.cpp
+++ countSubsequenceSumK.cpp
@@ -1,44 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-// T.C => n * (2**n)
-// S.C => O(n)
-
-int seq(int ind, int s, int sum, int arr[], int n) {
-	// condition is not satisfied
-	// strictly if arr is contains only positive integer
+int seq(int ind, int s, int sum, int n, int arr[]) {
 	if(s > sum) return 0;
-	if(ind == n) {
-		// condition satisfied
-		if(s == sum) return 1;
-
-		// not satisfied
-		else return 0;	
-
+	if(ind==n) {
+		if(s==sum) return 1;
+		else return 0;
 	}
-
-
 	
-	// Not pick the element in the subsequence
-	int l = seq(ind+1, s, sum, arr, n);
-
-
-	// pick the element in the subsequence
-    s += arr[ind];
-
-	int r = seq(ind+1, s, sum, arr, n);
-
+	// pick up the element
+	s += arr[ind];
+	int l = seq(ind+1, s, sum, n, arr);
 	s -= arr[ind];
+
+	// not pickup the element
+	int r = seq(ind+1, s, sum, n, arr);
 
 	return l+r;
 }
 
 int main() {
 	int arr[] = {1,2,1};
-	int n = sizeof(arr) / sizeof(arr[0]);
+	int n = sizeof(arr)/sizeof(arr[0]);
 	int sum = 2;
-	cout << seq(0, 0, sum, arr, n);
-
-	return 0;
+	cout << seq(0, 0, sum, n, arr);
 }
